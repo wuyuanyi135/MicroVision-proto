eb8c5cf5954caaa56fa459f0609172f6dc90d814
wuyuanyi <wuyuanyi135@gmail.com>
2019-04-13 15:42:14 -0400

    single instance driver release

--- protoc.sh
+++ protoc.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+rm $GOPATH/src/github.com/wuyuanyi135/mvprotos -r
 for service in ./*/
 do
 protoc \
     --go_out=plugins=grpc:$GOPATH/src \
     -I ./mvcgi \
-    -I ./mvcamctrl \
+    -I ./mvpulse \
     -I ./mvcam \
     ./`basename $service`/*.proto
 done
